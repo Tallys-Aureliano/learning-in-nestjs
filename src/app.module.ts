0e2dd99d58509eaeb5034ca0b5f413a5561ecda3
tallys-aureliano <tallysaureliano@outlook.com>
2022-09-24 18:07:07 -0300

    main: importações automaticas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TestController } from './test/test.controller';
+import { DatabseService } from './databse/databse.service';
 
 //decorators
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, TestController],
+  providers: [AppService, DatabseService],
 })
 export class AppModule {}
