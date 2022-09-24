2f4b2ec3be697dbfd61b5cd3ce93c4dac791acc0
tallys-aureliano <tallysaureliano@outlook.com>
2022-09-24 18:05:25 -0300

    main: fazendo comentarios

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('prefixo') //prefixo do controller
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('teste') ///get
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('teste2') ///get
+  acao(): string {
+    return 'Testando o modulos';
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
-@Injectable()
+@Injectable() // é um decoretor tbm, e significa que ele pode ser injetavel em outros logares ou seja podemos utiliar ele em outros locais
 export class AppService {
   getHello(): string {
     return 'Hello World!';
