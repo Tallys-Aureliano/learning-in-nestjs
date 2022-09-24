515e8e75624028020dd373e02d7fcecc6c27290f
tallys-aureliano <tallysaureliano@outlook.com>
2022-09-24 17:00:02 -0300

    main:fazendo comenatrios sobre o codigo.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
+// o codigo inicia aqui
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // criando instncia do modulo
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
